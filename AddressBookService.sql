d1a1fb2284457c4e2f099b0fd0dce2222d896288
namratapatil1957 <namratapatil1957@gmail.com>
2023-04-06 15:42:47 +0530

    Created a Table for AddressBook Database

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,3 +1,19 @@
 #UC1
 create database AddressBookService;
 use AddressBookService;
+
+#UC2
+create table AddressBook(
+id int not null auto_increment,
+firstname varchar(50) not null,
+lastname varchar(50) not null,
+address varchar(50) not null,
+city varchar(50) not null,
+state varchar(50) not null,
+zip int(6) not null,
+phone int(11) not null,
+email varchar(50) not null,
+primary key(id)
+);
+
+desc AddressBook;
